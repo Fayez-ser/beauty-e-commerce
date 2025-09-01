f8cd567e375c83f4b6c7454db0658b6d470de42a
Fayez-Serawan <serawanfayez@gmail.com>
2025-09-01 15:02:54 +0300

    finally

--- js/main.js
+++ js/main.js
@@ -2,11 +2,21 @@ let productsContainer = document.getElementById('products-container');
 let searchProductInput = document.querySelector('[name="search-product"]');
 let searchWishProductInput = document.querySelector('[name="search-wish-product"]');
 let wishListBody = document.getElementById('wish-list-body');
-let closeWishListBtn = document.getElementById('close-wish-btn');
-let closeBtns = document.querySelectorAll('.close-btn');
-let displayWishBtn = document.getElementById('display-wishlist-btn');
+let backgroundWish = document.getElementById('wish-is-empty');
 let wishListContainer = document.getElementById('wish-list-container');
-let wishListCounter = document.getElementById("wish-list-counter");
+let wishListCounter = document.querySelectorAll(".wish-list-counter");
+let displayWishBtn = document.getElementById('display-wishlist-btn');
+let cartProductsBody = document.getElementById('cart-products-body');
+let backgroundCart = document.getElementById('cart-is-empty');
+let cartProductContainer = document.getElementById('cart-products-container');
+let cartCounter = document.querySelectorAll(".cart-counter");
+let displayCartBtn = document.getElementById('display-cart-btn');
+let closeBtns = document.querySelectorAll('.close-btn');
+let netPayment = document.getElementById("net-payable");
+let netPaymentContainer = document.getElementById("payment-container");
+
+// let cartCounter = document.getElementById("cart-counter");
+// let closeWishListBtn = document.getElementById('close-wish-btn');
 
 
 function normalizeProduct(p) {
@@ -114,7 +124,7 @@ function renderProducts(arr, itemsContainer) {
                     class="hidden rounded-lg py-1 px-2 text-sm bg-warning text-white absolute bottom-4 left-4"
                     id="is-discount">${-1 * el.discountPercentage}</span>
                     <div class="absolute top-4 right-4" id="card-btns">
-                        <button class="edit-product cursor-pointer bg-light/50 hover:bg-white p-1 rounded-md text-primary">
+                        <button class="add-to-cart-btn edit-product cursor-pointer bg-light/50 hover:bg-white p-1 rounded-md text-primary">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
                                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                                 class="lucide lucide-square-pen size-6">
@@ -135,20 +145,20 @@ function renderProducts(arr, itemsContainer) {
                     </div>
                 </div>
                                 
-                <div class=" px-4 pt-10 pb-8 leading-8 flex-2" id="product-info">
-                    <p class="text-sm font-semibold" id="title">
+                <div class="product-info px-4 pt-10 pb-8 leading-8">
+                    <p class="text-sm font-semibold flex-1">
                     ${el.title}
                     </p>
                     
-                    <p class="text-lg font-semibold" id="brand">
+                    <p class="text-lg font-semibold">
                     <span class="font-bold text-primary">Brand </span>: ${el.brand}
                     </p>
                     
-                    <p class="text-primary/70 text-sm" id="description">
+                    <p class="text-primary/70 text-sm flex-2">
                         ${el.description}
                     </p>
                     
-                    <div id="rating-stars">
+                    <div class="rating-stars">
                         <i class="fa-solid fa-star fa-sm text-warning" data-rating></i>
                         <i class="fa-solid fa-star fa-sm text-warning" data-rating></i>
                         <i class="fa-solid fa-star fa-sm text-warning" data-rating></i>
@@ -164,36 +174,37 @@ function renderProducts(arr, itemsContainer) {
                         <p class="hidden text-sm bg-light rounded-lg py-1 px-4 " id="left-quantity">5 left</p>
                     </div>
                     <div class="product-btns">
-                    <button
-                        class="view-details-btn bg-primary hover:bg-primary/90 mt-8 outline-none py-2 text-sm font-bold rounded-md text-white flex items-center justify-center gap-1 cursor-pointer w-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
-                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
-                            class="lucide lucide-eye-icon lucide-eye">
-                            <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/>
-                            <circle cx="12" cy="12" r="3"/>
-                        </svg>
-                        <p>View Details</p>
-                    </button>
-                </div>
+                        <button
+                            class="view-details-btn bg-primary hover:bg-primary/90 mt-8 outline-none py-2 text-sm font-bold rounded-md text-white flex items-center justify-center gap-1 cursor-pointer w-full">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+                                class="lucide lucide-eye-icon lucide-eye">
+                                <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/>
+                                <circle cx="12" cy="12" r="3"/>
+                            </svg>
+                            <p>View Details</p>
+                        </button>
+                    </div>
             </div>`
 
 
     });
 };
 
+
 function renderWishProducts(arr) {
     wishListBody.innerHTML = '';
     arr.forEach((el, index) => {
         el.id = `wish-${index}`
         wishListBody.innerHTML += `
-            <div class="parent-close card w-full shadow-sm shadow-secondary bg-white rounded-2xl p-4 mb-3" data-close = ${index}>
+            <div class="parent-close card w-full shadow-sm shadow-secondary bg-white rounded-2xl p-4 mb-3" data-close = ${index} data-position = ${el.positionOriginal}>
                 <div class="card-body flex items-center gap-4 mb-3">
                     <div
                         class="main-image aspect-[1/1] object-cover rounded-2xl overflow-hidden shadow-sm shadow-primary/70">
                         <img class="w-full h-full" src="${el.images[0]}" alt="">
                     </div>
 
-                    <div class="leading-8" id="product-info">
+                    <div class="wish-product-info leading-8">
                         <p class="text-xs font-semibold">
                             ${el.title}
                         </p>
@@ -240,13 +251,19 @@ function renderWishProducts(arr) {
             </div>
         `
     });
+
     updateCounter(wishListCounter, wishListProducts);
     let removeFromWishBtn = document.querySelectorAll(".remove-from-wish-btn");
+    let addToWishBtns = document.querySelectorAll(".add-to-favorite-btn");
+
     removeFromWishBtn.forEach(btn => {
         btn.addEventListener("click", () => {
             let parentCard = btn.closest(".parent-close");
             let position = parentCard.getAttribute("data-close");
+            let positionOriginal = parentCard.getAttribute("data-position");
+            toggleHeart(addToWishBtns[positionOriginal]);
             wishListProducts.splice(position, 1);
+            toggleBackground(wishListProducts, backgroundWish);
             renderWishProducts(wishListProducts);
         });
     });
@@ -254,18 +271,174 @@ function renderWishProducts(arr) {
 
 }
 
-function updateCounter(counter, arr) {
-    if (arr.length > 0) {
-        counter.classList.replace("hidden", "flex");
+function renderCartProducts(arr) {
+    cartProductsBody.innerHTML = '';
+    arr.forEach((el, index) => {
+        el.id = `cart-${index}`
+        cartProductsBody.innerHTML += `
+            <div class="parent-close card w-full shadow-sm shadow-secondary bg-white rounded-2xl p-4 mb-3"
+                data-close="${index}" data-position=${el.positionOriginal}>
+                <div class="card-body flex items-center gap-4 mb-3">
+                    <div
+                        class="main-image aspect-[1/1] w-25 h-25 rounded-2xl overflow-hidden shadow-sm shadow-primary/70">
+                        <img class="w-full h-full" src="${el.images[0]}" alt="">
+                    </div>
+                    <div class=" cart-product-info leading-8">
+                        <p class="text-xs font-semibold">
+                            ${el.title}
+                        </p>
+                        <p class="text-md font-semibold">
+                            <span class="font-bold text-primary">Brand </span>: ${el.brand}
+                        </p>
+                        <div class=" flex justify-between items-center" id="Purchase-information">
+                            <div class="flex gap-3 items-center" id="Prices">
+                                <p class="text-xl font-bold text-purple" id="final-price">$${parseFloat((el.price - (el.discountPercentage * el.price / 100)).toFixed(2))}</p>
+                                <p class="text-xs text-primary/70 line-through" id="original-price">$${el.price}</p>
+                            </div>
+                            <p class="hidden text-xs bg-light rounded-lg py-1 px-4 " id="left-quantity">5 left
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="quantity-price-container flex items-center justify-between">
+                    <div class="cart-quantity-btns flex gap-3 items-center justify-center">
+                        <button
+                            class="decrease-quantity-btn px-2 py-2 bg-danger hover:bg-danger/90 outline-none text-xs font-semibold rounded-md text-white cursor-pointer" data-stock="-">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="lucide lucide-square-minus-icon lucide-square-minus">
+                                <rect width="18" height="18" x="3" y="3" rx="2"></rect>
+                                <path d="M8 12h8"></path>
+                            </svg>
+                        </button>
+                        <p class="quantity-container font-semibold tetx-xl "><span class="quantity">1</span><span> Pcs</span></p>
+                        <button
+                            class="increase-quantity-btn px-2 py-2 bg-green-400 hover:bg-green-500 outline-none text-xs font-semibold rounded-md text-white cursor-pointer" data-stock="+">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="lucide lucide-square-plus-icon lucide-square-plus">
+                                <rect width="18" height="18" x="3" y="3" rx="2"></rect>
+                                <path d="M8 12h8"></path>
+                                <path d="M12 8v8"></path>
+                            </svg>
+                        </button>
+                    </div>
+                    <p class="total-price-container font-bold text-2xl underline underline-offset-8">
+                        <span>$</span><span class="total-price">${parseFloat((el.price - (el.discountPercentage * el.price / 100)).toFixed(2))}</span>
+                    </p>
+                </div>
+            </div>
+        `
+    });
+
+    updateCounter(cartCounter, cartProducts);
+    // let addToWishBtns = document.querySelectorAll(".add-to-favorite-btn");
+    let removeFromCartBtn = document.querySelectorAll(".remove-from-cart-btn");
+    let increaseQuantityBtns = document.querySelectorAll(".increase-quantity-btn");
+    let decreaseQuantityBtns = document.querySelectorAll(".decrease-quantity-btn");
+    let quantityInCart = document.querySelectorAll(".quantity");
+    let totalPriceForEachProduct = document.querySelectorAll(".total-price");
+
+
+
+
+    removeFromCartBtn.forEach(btn => {
+        btn.addEventListener("click", () => {
+            let parentCard = btn.closest(".parent-close");
+            let position = parentCard.getAttribute("data-close");
+            let positionOriginal = parentCard.getAttribute("data-position");
+            // toggleHeart(addToWishBtns[positionOriginal]);
+            cartProducts.splice(position, 1);
+            toggleBackground(cartProducts, backgroundCart);
+            renderCartProducts(cartProducts);
+        });
+    });
+    let netPayementArr = [];
+    let currentStock;
+    let finalUnitPrice;
+
+    increaseQuantityBtns.forEach((stockBtn, index) => {
+        netPayementArr = [...totalPriceForEachProduct].map((e) => parseFloat(e.textContent));
+        netPayment.textContent = netPayementArr.reduce((previous, current) => previous + current, 0);
+
+        stockBtn.addEventListener("click", () => {
+            netPayementArr = [];
+            currentStock = parseInt(quantityInCart[index].textContent);
+            quantityInCart[index].textContent = currentStock + 1;
+
+            let unitPrice = cartProducts[index].price - (cartProducts[index].discountPercentage * cartProducts[index].price / 100);
+            finalUnitPrice = (currentStock + 1) * unitPrice;
+            totalPriceForEachProduct[index].textContent = finalUnitPrice.toFixed(2);
+            netPayementArr = [...totalPriceForEachProduct].map(e => parseFloat(e.textContent));
+            netPayment.textContent = netPayementArr.reduce((prev, curr) => prev + curr, 0).toFixed(2);
+
+        });
+
+    })
+
+    decreaseQuantityBtns.forEach((stockBtnMinus, index) => {
+        netPayementArr = [...totalPriceForEachProduct].map((e) => parseFloat(e.textContent));
+        netPayment.textContent = netPayementArr.reduce((previous, current) => previous + current, 0);
+
+        stockBtnMinus.addEventListener("click", () => {
+    currentStock = parseInt(quantityInCart[index].textContent);
+
+    if (currentStock > 1) {
+        quantityInCart[index].textContent = currentStock - 1;
+
+        let unitPrice = cartProducts[index].price * (1 - cartProducts[index].discountPercentage / 100);
+        let finalUnitPrice = (currentStock - 1) * unitPrice;
+        totalPriceForEachProduct[index].textContent = finalUnitPrice.toFixed(2);
+
+        netPayementArr = [...totalPriceForEachProduct].map(e => parseFloat(e.textContent));
+        netPayment.textContent = netPayementArr.reduce((prev, curr) => prev + curr, 0).toFixed(2);
+
+    } else {
+        cartProducts.splice(index, 1);
+        renderCartProducts(cartProducts);
+        toggleBackground(cartProducts, backgroundCart);
+    }
+});
+
+    })
+    if (cartProducts.length > 0) {
+        netPaymentContainer.classList.replace("hidden", "flex");
+
     }
     else {
-        counter.classList.replace("flex", "hidden");
+        netPaymentContainer.classList.replace("flex", "hidden");
+
     }
-    counter.textContent = arr.length;
+
+}
+
+function updateCounter(counter, arr) {
+    counter.forEach(co => {
+        if (arr.length > 0) {
+            co.classList.replace("hidden", "flex");
+        }
+        else {
+            co.classList.replace("flex", "hidden");
+        }
+        co.textContent = arr.length;
+    })
 }
 
 function toggleHeart(btn) {
     btn.firstElementChild.classList.toggle("fill-danger");
+}
+
+function toggleProduct(newArray, oldArray, indexInOldArray, renderFun) {
+    positionInProductsArr = indexInOldArray;
+    let newProduct = oldArray[positionInProductsArr];
+    newProduct.positionOriginal = positionInProductsArr;
+    let isProductContain = newArray.findIndex(o => o.id === newProduct.id)
+    if (isProductContain === -1) {
+        newArray.push(newProduct);
+    } else {
+        newArray.splice(isProductContain, 1);
+    }
+    renderFun(newArray);
 }
 
 
@@ -303,6 +476,7 @@ function searchAllProducts(arr, searchContainer, renderFunction, searchInput) {
 
 /* Call renderProducts Function to display all product card on home page */
 let wishListProducts = [];
+let cartProducts = [];
 init().then(products => {
     renderProducts(products, productsContainer);
 
@@ -310,33 +484,28 @@ init().then(products => {
     addToWishBtns.forEach((item, index) => {
         item.addEventListener("click", () => {
             toggleHeart(item);
-            positionInProductsArr = index;
-            let newWishProduct = products[positionInProductsArr];
-            let isProductContain = wishListProducts.findIndex(o => o.id === newWishProduct.id)
-            if (isProductContain === -1) {
-                wishListProducts.push(newWishProduct);
-            } else {
-                wishListProducts.splice(isProductContain, 1);
-            }
-            renderWishProducts(wishListProducts);
-
-
+            toggleProduct(wishListProducts, products, index, renderWishProducts);
+            toggleBackground(wishListProducts, backgroundWish)
         });
     });
 
-
+    let addToCartBtns = document.querySelectorAll(".add-to-cart-btn");
+    addToCartBtns.forEach((item, index) => {
+        item.addEventListener("click", () => {
+            toggleProduct(cartProducts, products, index, renderCartProducts);
+            toggleBackground(cartProducts, backgroundCart);
+        });
+    });
 });
 
-// removeFromWishBtn.forEach((e) => {
-//     e.addEventListener("click", () => {
-//         let position = e.getAttribute("data-close");
-//         console.log(wishListProducts[position]);
-
-//     })
-// })
-
-
-
+function toggleBackground(arr, background) {
+    if (arr.length > 0) {
+        background.classList.add("hidden");
+    }
+    else {
+        background.classList.remove("hidden");
+    }
+}
 
 searchProductInput.addEventListener("input", () => {
     init().then(products => {
@@ -347,12 +516,15 @@ searchProductInput.addEventListener("input", () => {
 
 searchWishProductInput.addEventListener("input", () => {
     searchAllProducts(wishListProducts, wishListBody, renderWishProducts, searchWishProductInput);
-    console.log("yes");
-
 });
 
 displayWishBtn.addEventListener("click", () => {
     wishListContainer.classList.replace("hidden", "flex");
+
+});
+displayCartBtn.addEventListener("click", () => {
+    cartProductContainer.classList.replace("hidden", "flex");
+    // cartProductContainer.classList.remove("hidden");
 
 });
 
